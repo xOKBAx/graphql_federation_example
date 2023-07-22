f99d6eae3c3c0d47c9df3a9116b2e2b2c857a14a
Okba Boularaoui <okba2fb@gmail.com>
2023-07-23 01:27:59 +0300

    support mutations

--- main.py
+++ main.py
@@ -12,16 +12,20 @@ class Book: # pylint: disable=too-few-public-methods
     author: str
 
 
+# Database
+database = [
+    Book(title="The Great Gatsby", author="F. Scott Fitzgerald"),
+    Book(title="Gatsby 2", author="F. Scott Fitzgerald"),
+]
+
+
 # Strawberry can work with any data source
 # (for example a database, a REST API, files, etc).
 # For this tutorial we will be using hard-coded data.
 
 def get_books():
     """Simulation of a data getter (Like database for example)"""
-    return [
-        Book(title="The Great Gatsby", author="F. Scott Fitzgerald"),
-        Book(title="Gatsby 2", author="F. Scott Fitzgerald"),
-    ]
+    return database
 
 
 # Strawberry doesn’t know it should use "get_books" when executing a query.
@@ -34,20 +38,35 @@ class Query: # pylint: disable=too-few-public-methods
     books: list[Book] = strawberry.field(resolver=get_books)
 
 
+@strawberry.type
+class Mutation: # pylint: disable=too-few-public-methods
+    """Mutation Type"""
+    @strawberry.mutation
+    def add_book(self, title: str, author: str) -> Book:
+        """Add more books"""
+        book = Book(title=title, author=author)
+        database.append(book)
+        return book
+
+
 # We have defined our data and query,
 # now what we need to do is create a GraphQL schema and start the server.
 # Command: strawberry server main
 # http://localhost:8000/graphql
 
-schema = strawberry.Schema(query=Query)
+schema = strawberry.Schema(query=Query, mutation=Mutation)
 
 
 # Now we can run the next query to retrieve all books
 """
-{
+query books {
   books {
     title
-    author
+  	author
   }
 }
+"""
+# Or this to create a new book
+"""
+
 """
